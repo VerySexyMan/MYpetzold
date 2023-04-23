5fc96abe6e37ecd3285baea106d2be757b6be847
Arkady Aminov <57907972+VerySexyMan@users.noreply.github.com>
2023-04-23 18:32:25 +0500

    Update ShapeAnEllipse.cs

--- ShapeAnEllipse.cs
+++ ShapeAnEllipse.cs
@@ -26,7 +26,7 @@ namespace Petzold.ShapeAnEllipse
             Ellipse elips = new Ellipse(); // Инициализирует новый экземпляр класса Ellipse.
             elips.Fill = Brushes.AliceBlue(); // Получает или задает структуру Brush, определяющую способ рисования внутренней области фигуры.
             elips.StrokeThickness = 24; // 1/4 дюйма // Получает или задает ширину контура Shape.
-            elips.Stroke = new LinearGradientBrush(Colors.CadetBlue, Colors.Chocolate.new Point(1. 0), new Point(0. 1)); // создание объекта "кисть" (аргументы - начальный цвет, конечный цвет, угол) // первые два аргумента - это диапазон цвета градиента
+            elips.Stroke = new LinearGradientBrush(Colors.CadetBlue, Colors.Chocolate.new Point(1, 0), new Point(0, 1)); // создание объекта "кисть" (аргументы - начальный цвет, конечный цвет, угол) // первые два аргумента - это диапазон цвета градиента
           Content = elips; /// экземпляр класса Ellipse задается свойству Content окна
         }
     }
