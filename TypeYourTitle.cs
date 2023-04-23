04c1b3a10603d2f17567543a56924cc5c509a0b9
Arkady Aminov <57907972+VerySexyMan@users.noreply.github.com>
2023-04-23 17:49:33 +0500

    TypeYourTitle.cs

---
+++ TypeYourTitle.cs
@@ -0,0 +1,28 @@
+//---------------------------------------------- 
+// TypeYourTitle.cs (c) 2006 by Charles Petzold 
+//----------------------------------------------
+
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Petzold.TypeYourTitle
+{
+    public class TypeYourTitle : Window //создан класс TypeYourTitle типа Window
+    {         
+        [STAThread]         
+        public static void Main()         
+        {             
+            Application app = new Application(); //создает объект app класса Appliation           
+            app.Run(new TypeYourTitle()); 
+        }         
+        protected override void OnTextInput (TextCompositionEventArgs args)         
+        {             
+            base.OnTextInput(args);             
+            if (args.Text == "\b" && Title.Length > 0) // применяем метод Backspace, если есть хотя бы один символ                 
+                Title = Title.Substring(0, Title .Length - 1); //нажатие Backspace приводит к удалению символа из строки           
+          else if (args.Text.Length > 0 && !Char .IsControl(args.Text[0]))//  в противном случае                
+                Title += args.Text; // присоединение текста к содержимому 
+        }     
+    } 
+} 
